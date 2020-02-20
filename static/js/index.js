765c6a621680e81e5b90de3b208f53016b43e688
cjj <junjianch@gamil.com>
2020-02-20 22:15:27 +0800

    弹窗

--- static/js/index.js
+++ static/js/index.js
@@ -248,14 +248,14 @@ function init() {
     maxSelectionCount: 1,
     nodeTemplateMap: myDiagram.nodeTemplateMap,
     model: new go.GraphLinksModel([
-      { name: "NoOperation", code: "ss", type: 'No Operation', source: "static/img/NoOperation.gif" },
-      { name: "Encoding", code: "ss", type: 'Encoding', source: "static/img/Encoding.gif" },
-      { name: "Json2Xml", code: "ss", type: 'Json2Xml', source: "static/img/Json2Xml.gif" },
-      { name: "Xml2Json", code: "ss", type: 'Xml2Json', source: "static/img/Xml2Json.gif" },
-      { name: "Template", code: "ss", type: 'Template', source: "static/img/Template.gif" },
-      { name: "Datebase", code: "ss", type: 'Datebase', source: "static/img/Datebase.gif" },
-      { name: "HttpClient", code: "ss", type: 'Http Client', source: "static/img/HttpClient.gif" },
-      { name: "SoapClient", code: "ss", type: 'Soap Client', source: "static/img/SoapClient.gif" }
+      { name: "NoOperation", code: "", type: 'NoOperation', source: "static/img/NoOperation.gif" },
+      { name: "Encoding", code: "", type: 'Encoding', source: "static/img/Encoding.gif" },
+      { name: "Json2Xml", code: "", type: 'Json2Xml', source: "static/img/Json2Xml.gif" },
+      { name: "Xml2Json", code: "", type: 'Xml2Json', source: "static/img/Xml2Json.gif" },
+      { name: "Template", code: "", type: 'Template', source: "static/img/Template.gif" },
+      { name: "Datebase", code: "", type: 'Datebase', source: "static/img/Datebase.gif" },
+      { name: "HttpClient", code: "", type: 'HttpClient', source: "static/img/HttpClient.gif" },
+      { name: "SoapClient", code: "", type: 'SoapClient', source: "static/img/SoapClient.gif" }
     ])
   });
 }
@@ -291,3 +291,82 @@ function loadDiagramProperties() {
   var pos = myDiagram.model.modelData.position;
   if (pos) myDiagram.initialPosition = go.Point.parse(pos);
 }
+
+function showDialog(templateData) {
+  console.log(templateData);
+  $("#ItemName").val(templateData.name)
+  $("#TemplateCode").val(templateData.code)
+  $.ligerDialog.open({
+    // target: $("#dialog"),
+    title: "添加控件"+ templateData.type,
+    url: './add' + templateData.type + '.html',
+    width: 750,
+    height: 480,
+    isResize: true,
+    modal: true,
+    buttons: [
+      { text: '关闭窗口', onclick: function (item, dialog) { dialog.hide(); } },
+      { text: '提交', onclick: function (item, dialog) { 
+        flash($("#ItemName").val(),$("#TemplateCode").val(),templateData)
+        $.ligerDialog.hide()
+       } }
+    ]
+  })
+}
+  // $(function () {
+  //   $.metadata.setType("attr", "validate");
+  //   var v = $("#form1").validate({
+  //     debug: true,
+  //     errorPlacement: function (lable, element) {
+  //       if (element.hasClass("l-textarea")) {
+  //         element.ligerTip({content: lable.html(), target: element[0]});
+  //       } else if (element.hasClass("l-text-field")) {
+  //         element.parent().ligerTip({content: lable.html(), target: element[0]});
+  //       } else {
+  //         lable.appendTo(element.parents("td:first").next("td"));
+  //       }
+  //     },
+  //     success: function (lable) {
+  //       lable.ligerHideTip();
+  //       lable.remove();
+  //     },
+  //     submitHandler: function (e) {
+  //       // document.form1.submit()
+  //       flash($("#ItemName").val(),$("#TemplateCode").val(),templateData)
+  //       // dialog.hide()
+  //       $.ligerDialog.hide()
+  //     }
+  //   });
+  //   $("#form1").ligerForm();
+  //   $(".l-button-test").click(function () {
+  //     alert(v.element($("#txtName")));
+  //   });
+  // });
+function flash(newItemName, newTemplateCode, oldTemplateData) {
+  var model = myDiagram.model;
+  //所有模型更改都应该在事务模型中进行
+  model.startTransaction("flash");
+  var data = model.nodeDataArray.filter( x => {
+    return x.key === oldTemplateData.key
+  })[0]
+  model.setDataProperty(oldTemplateData, "name", newItemName); // 修改属性
+  model.setDataProperty(oldTemplateData, "code", newTemplateCode); // 修改属性
+  model.commitTransaction("flash");
+}
+function  test(){
+  console.log('hi');
+  
+}
+// var upflag = {{.errormsg}};
+
+// if (upflag != "") {
+//   if (upflag == "保存成功") {
+//     parent.window.loadtree();
+//     $(".l-bar-button.l-bar-btnload", window.parent.document).click();
+//     parent.$.ligerDialog.close();
+//     parent.$(".l-dialog,.l-window-mask").remove();
+
+//   } else {
+//     alert(upflag);
+//   }
+// }
--- static/js/showDialog.js
+++ static/js/showDialog.js
@@ -2,8 +2,9 @@ function showDialog(templateData) {
   $("#ItemName").val(templateData.name)
   $("#TemplateCode").val(templateData.code)
   $.ligerDialog.open({
-    target: $("#dialog"),
+    // target: $("#dialog"),
     title: "添加控件"+ templateData.type,
+    url: './add' + templateData.type + '.html',
     width: 750,
     height: 480,
     isResize: true,
@@ -12,47 +13,47 @@ function showDialog(templateData) {
       { text: '取消', onclick: function (item, dialog) { dialog.hide(); } }
     ]
   });
-  $(function () {
-    $.metadata.setType("attr", "validate");
-    var v = $("#form1").validate({
-      debug: true,
-      errorPlacement: function (lable, element) {
-        if (element.hasClass("l-textarea")) {
-          element.ligerTip({content: lable.html(), target: element[0]});
-        } else if (element.hasClass("l-text-field")) {
-          element.parent().ligerTip({content: lable.html(), target: element[0]});
-        } else {
-          lable.appendTo(element.parents("td:first").next("td"));
-        }
-      },
-      success: function (lable) {
-        lable.ligerHideTip();
-        lable.remove();
-      },
-      submitHandler: function (e) {
-        // document.form1.submit()
-        flash($("#ItemName").val(),$("#TemplateCode").val(),templateData)
-        // dialog.hide()
-        $.ligerDialog.hide()
-      }
-    });
-    $("#form1").ligerForm();
-    $(".l-button-test").click(function () {
-      alert(v.element($("#txtName")));
-    });
-  });
+  // $(function () {
+  //   $.metadata.setType("attr", "validate");
+  //   var v = $("#form1").validate({
+  //     debug: true,
+  //     errorPlacement: function (lable, element) {
+  //       if (element.hasClass("l-textarea")) {
+  //         element.ligerTip({content: lable.html(), target: element[0]});
+  //       } else if (element.hasClass("l-text-field")) {
+  //         element.parent().ligerTip({content: lable.html(), target: element[0]});
+  //       } else {
+  //         lable.appendTo(element.parents("td:first").next("td"));
+  //       }
+  //     },
+  //     success: function (lable) {
+  //       lable.ligerHideTip();
+  //       lable.remove();
+  //     },
+  //     submitHandler: function (e) {
+  //       // document.form1.submit()
+  //       flash($("#ItemName").val(),$("#TemplateCode").val(),templateData)
+  //       // dialog.hide()
+  //       $.ligerDialog.hide()
+  //     }
+  //   });
+  //   $("#form1").ligerForm();
+  //   $(".l-button-test").click(function () {
+  //     alert(v.element($("#txtName")));
+  //   });
+  // });
 }
-function  flash(newItemName, newTemplateCode, oldTemplateData) {
-  var model = myDiagram.model;
-  //所有模型更改都应该在事务模型中进行
-  model.startTransaction("flash");
-  var data = model.nodeDataArray.filter( x => {
-    return x.key === oldTemplateData.key
-  })[0]
-  model.setDataProperty(oldTemplateData, "name", newItemName); // 修改属性
-  model.setDataProperty(oldTemplateData, "code", newTemplateCode); // 修改属性
-  model.commitTransaction("flash");
-}
+// function  flash(newItemName, newTemplateCode, oldTemplateData) {
+//   var model = myDiagram.model;
+//   //所有模型更改都应该在事务模型中进行
+//   model.startTransaction("flash");
+//   var data = model.nodeDataArray.filter( x => {
+//     return x.key === oldTemplateData.key
+//   })[0]
+//   model.setDataProperty(oldTemplateData, "name", newItemName); // 修改属性
+//   model.setDataProperty(oldTemplateData, "code", newTemplateCode); // 修改属性
+//   model.commitTransaction("flash");
+// }
 
 // var upflag = {{.errormsg}};
 
